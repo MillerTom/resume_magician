d848b0a7b5be652ace751813dfa8b1d1d3c13bca
andriimykytiuk1121 <87443982+andriimykytiuk1121@users.noreply.github.com>
2025-02-24 17:47:22 -0500

    Update manage.py
    extra edit

--- server/manage.py
+++ server/manage.py
@@ -26,7 +26,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    runserver.default_port = 5000
+    runserver.default_port = 80
     execute_from_command_line(sys.argv)
 
 
