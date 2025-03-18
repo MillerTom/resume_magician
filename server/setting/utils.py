d13e016889231309c98af00cfd9f5800bbf2c45c
rebmxict <rebmxict@gmail.com>
2025-03-18 20:19:18 +0800

    datasource default value

--- server/setting/utils.py
+++ server/setting/utils.py
@@ -2,6 +2,9 @@ from setting.models import DataSource
 
 def get_datasource():
     datasource = DataSource.objects.first()
+    if not datasource:
+        datasource = DataSource(value='google_sheet')
+        datasource.save()
     return datasource.value
 
 def is_google_sheet():
